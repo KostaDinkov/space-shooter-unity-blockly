f63f263f872d17e9d46566a522754a060b1118b9
Kosta <k.v.dinkov@gmail.com>
2018-11-15 14:51:19 +0200

    add: asteroid material updates automatically based on the isDestroyable flag

---
+++ Assets/Scripts/SpaceObject/IAsteroid.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using Game.SpaceObject;
+using UnityEditor;
+
+
+[ExecuteInEditMode]
+public class IAsteroid : MonoBehaviour
+{
+    private Transform asteroid;
+    private Renderer rend;
+    private Material blackMaterial;
+    private Material defaultMaterial;
+    private string asteroidName;
+
+    void Awake()
+    {
+        asteroid = transform.GetChild(0);
+        if (asteroid == null)
+        {
+            return;
+        }
+        rend = asteroid.gameObject.GetComponent<Renderer>();
+        name = asteroid.name;
+        Debug.Log($"model name:{name}");
+        blackMaterial = Resources.Load<Material>($"LoadableMaterials/{name}_mat_black");
+        defaultMaterial = Resources.Load<Material>($"LoadableMaterials/{name}_mat");
+
+        ApplyMaterial();
+
+    }
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Updates material while in edit mode. In play mode we do not need to apply material constantly;
+        if (!EditorApplication.isPlaying)
+        {
+            ApplyMaterial();
+        }
+        
+    }
+
+    public void ApplyMaterial()
+    {
+       if (!this.GetComponent<ISpaceObject>().IsDestroyable)
+        {
+            
+            rend.material = blackMaterial;
+        }
+        else
+        {
+            rend.material = defaultMaterial;
+        }
+    }
+}
