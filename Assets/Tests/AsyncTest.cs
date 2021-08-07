7dbee7582ec070faf924d4a371dbc088b30d59a6
Kosta <k.v.dinkov@gmail.com>
2021-08-07 14:17:35 +0300

    write some play mode integration tests

---
+++ Assets/Tests/AsyncTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+
+
+namespace Tests
+{
+    public class AsyncTest
+    {
+        public static IEnumerator Execute<T>(Task<T> task)
+        {
+            while (!task.IsCompleted)
+            {
+                yield return null;
+            }
+
+            yield return task.Result;
+        }
+        public static IEnumerator Execute(Task task)
+        {
+            while (!task.IsCompleted)
+            {
+                yield return null;
+            }
+
+            if (task.IsFaulted) throw task.Exception;
+
+
+        }
+    }
+}
--- Assets/Tests/BasicMechanicsTests.cs
+++ Assets/Tests/BasicMechanicsTests.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using NUnit.Framework;
+using Scripts.Exceptions;
 using Scripts.Systems;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,36 +11,118 @@ using UnityEngine.TestTools;
 
 namespace Tests
 {
-    public class BasicMechanicsTests
+    public class Problem1BasicTests
     {
+        private Playercontroller player;
+
+        private GameController game;
+
         // A Test behaves as an ordinary method
         [Test]
         public void PlayerCollisionShouldMakePlayerDie()
         {
-           
+        }
+
+        [SetUp]
+        public async void Setup()
+        {
+            SceneManager.LoadScene(0);
+            await UniTask.NextFrame();
+            this.player = GameObject.Find("Player-Drone").GetComponent<Playercontroller>();
+            this.game = GameObject.Find("GameController").GetComponent<GameController>();
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator BasicMechanicsTestsWithEnumeratorPasses()
+        public IEnumerator PlayerShouldDieOnCollision()
         {
-            Debug.Log(SceneManager.sceneCountInBuildSettings);
-            AsyncOperation loadScene =  SceneManager.LoadSceneAsync(0);
-
-            while (!loadScene.isDone)
+            var controlTask = MovePlayerToCollide(player);
+            while (controlTask.Status == UniTaskStatus.Pending)
             {
                 yield return null;
             }
-            var player = GameObject.Find("Player-Drone").GetComponent<Playercontroller>();
-            var game = GameObject.Find("GameController").GetComponent<GameController>();
 
-            player.MoveForwardAsync();
+            Assert.IsTrue(game.IsPlayerDead);
+        }
+
+        [UnityTest]
+        public IEnumerator PlayerShouldNotDieOnPlatform()
+        {
+            var controlTask = MovePlayerToPlatform(player);
+            while (controlTask.Status == UniTaskStatus.Pending)
+            {
+                yield return null;
+            }
+
             Assert.IsTrue(!game.IsPlayerDead);
+        }
 
+        [UnityTest]
+        public IEnumerator CommandAfterPlayerDiedShouldThrow()
+        {
+
+            IEnumerator result;
+            var task = CommandsAfterPlayerDies(player);
+            do
+            {
+                result = AsyncTest.Execute(task);
+                yield return result;
+               
+            }
+            while((result.Current) == null);
+            
+            Assert.True(typeof(PlayerDiedException) == result.Current.GetType() );
             
 
+
+        }
+
+        private async Task<Exception> CommandsAfterPlayerDies(Playercontroller player)
+        {
+            try
+            {
+                await player.MoveForwardAsync();
+                await player.MoveForwardAsync();
+                await player.MoveForwardAsync();
+                await player.MoveForwardAsync();
+                await player.MoveForwardAsync();
+                throw new NotImplementedException();
+            }
             
+            catch (Exception ex)
+            {
+                return ex;
+            }
+            
+        }
+
+        private async UniTask MovePlayerToPlatform(Playercontroller player)
+        {
+            await player.RotateLeftAsync();
+            await player.MoveForwardAsync();
+            await player.RotateRightAsync();
+            await player.MoveForwardAsync();
+            await player.MoveForwardAsync();
+            await player.MoveForwardAsync();
+            await player.RotateLeftAsync();
+            await player.MoveForwardAsync();
+            await player.MoveForwardAsync();
+            await player.RotateRightAsync();
+            await player.MoveForwardAsync();
+            await player.MoveForwardAsync();
+            await player.MoveForwardAsync();
+            await player.MoveForwardAsync();
+            await player.RotateRightAsync();
+            await player.MoveForwardAsync();
+            await player.MoveForwardAsync();
+            await player.MoveForwardAsync();
+        }
+
+        private async UniTask MovePlayerToCollide(Playercontroller player)
+        {
+            await player.MoveForwardAsync();
+            await player.MoveForwardAsync();
         }
     }
 }
