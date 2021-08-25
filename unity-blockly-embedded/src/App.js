3a8c49adf7254097943162912f8a222b3ff950f2
Kosta <k.v.dinkov@gmail.com>
2021-08-25 21:52:23 +0300

    add blockly to the project

--- unity-blockly-embedded/src/App.js
+++ unity-blockly-embedded/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, {useEffect} from "react";
+import Blockly from 'blockly'
 import './App.css';
 
 function App() {
+  let blocklyRef = React.createRef();
+  let toolbox = {
+    "kind": "categoryToolbox",
+    "contents": [
+      {
+        "kind": "category",
+        "name": "Control",
+        "contents": [
+          {
+            "kind": "block",
+            "type": "controls_if"
+          },
+          {
+            "kind": "block",
+            "type": "controls_whileUntil"
+          },
+          {
+            "kind": "block",
+            "type": "controls_for"
+          }
+        ]
+      },
+      {
+        "kind": "category",
+        "name": "Logic",
+        "contents": [
+          {
+            "kind": "block",
+            "type": "logic_compare"
+          },
+          {
+            "kind": "block",
+            "type": "logic_operation"
+          },
+          {
+            "kind": "block",
+            "type": "logic_boolean"
+          }
+        ]
+      }
+    ]
+  }
+  
+  useEffect(()=>{
+    let workspace = Blockly.inject(blocklyRef.current,{toolbox});
+  });
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{height:"900px", width:"1000px"}} ref={blocklyRef}></div>
     </div>
   );
 }
